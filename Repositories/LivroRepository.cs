7e66cda1013735bc7f3cb4ae18a7839575f3a7eb
Welingnton <wellington902008@hotmail.com>
2022-08-28 11:01:11 -0300

    GET

---
+++ Repositories/LivroRepository.cs
@@ -0,0 +1,21 @@
+﻿using Chapter.Contexts;
+using Chapter.Models;
+
+namespace Chapter.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly SqlContext _context;
+
+        public LivroRepository(SqlContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+    }
+}
